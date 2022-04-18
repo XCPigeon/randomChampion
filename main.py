0b3cacc09cf327fa2440e913fa8d603ed713e7a9
XCPigeon <aprotyme@gmail.com>
2022-04-18 17:08:04 -0400

    Minor Changes
    -Changed the "device viewport" in the html thing because we dont need it and idk what it does and it works fine without it. 
    -Changed the text on the button

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ champ = 0
 def write_HTML(numb):  # this just writes to the html file so OBS can see it.
     file = open("number.html", "w")
     file.write("<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<title>Random Number</title>\n\t\t<meta "
-               "charset=\"UTF-8\">\n\t\t<meta name=\"viewport\" content=\"width=device-width, "
-               "initial-scale=1.0\">\n\t\t<meta http-equiv=\"refresh\" content=\"2\">\n\t</head>")
+               "charset=\"UTF-8\">\n\t\t<meta http-equiv=\"refresh\" content=\"2\">\n\t</head>")
     file.write("\n\t<body style=\"font-size:300%;\">\n\t\t<h1 style=\"color:red;\">")
     file.write(str(numb))
     file.write("</h1>\n\t</body>\n</html")
@@ -22,8 +21,8 @@ def random_champ():
     champ = random.randint(1, 54)  # a random int between 1-55
     return champ
 
-def do_this(): 
-    global champ, string                                # pull down the champion and  
+def do_this():
+    global champ, string                                # pull down the champion and
     champ = random_champ()                              # make a new random Champion
     champ_name = read_champ(champ)
     write_HTML(champ_name)                                   # push that to the HTML
@@ -32,7 +31,7 @@ def do_this():
     label.pack()
 
 def read_champ(num): # reads what champion is what number
-    # added a file called 'array.txt' that lists all the champions. 
+    # added a file called 'array.txt' that lists all the champions.
     texts = open("array.txt", "r") # open the file as read only
     while (num != 0):              # go through each line until we hit the line we want
         name = texts.readline()
@@ -45,8 +44,8 @@ def read_champ(num): # reads what champion is what number
 
 window = tk.Tk() # makes a window with the size below
 window.geometry("300x100")
-greeting = tk.Button(text="Hello there!", command=lambda: do_this()) # makes a button, then adds text
+greeting = tk.Button(text="New Champion!", command=lambda: do_this()) # makes a button, then adds text
 label = tk.Label(text="Press the button for a Random Champion")
 label.pack() # puts it all together and pops up a window
 greeting.pack()
-window.mainloop() 
+window.mainloop()
